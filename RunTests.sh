3f28c1aa66a3f567cd150079e36cd6b33a8a6df2
Axel Villanueva <axelvillanueva@pm.me>
2023-05-01 22:43:23 -0600

    Implementing last test cases files

--- RunTests.sh
+++ RunTests.sh
@@ -1,5 +1,5 @@
 #/bin/sh
-files=("original" "incremental" "fails" "variableDeclaredTwice" "test" "imposibleAssignment" "wrongIf" "simpleIf" "ifElse" "while")
+files=("original" "incremental" "fails" "variableDeclaredTwice" "test" "imposibleAssignment" "wrongIf" "simpleIf" "ifElse" "while" "fibonacci" "factorial")
 for i in ${!files[@]}; do
   echo "Test Case #$(expr $i + 1) (${files[$i]})"
   CUR="./examples/${files[$i]}.toy"
